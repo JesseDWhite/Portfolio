6c54a3b4f6962418fe867b2b0dbcebc5e28ce53d
Jesse White <jesse.white6@gmail.com>
2021-09-26 15:27:42 -0700

    add card style to projects component.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,52 +1,92 @@
 import React from 'react';
-import { Typography, Grid, Paper, Box } from '@mui/material';
+import { Typography, Grid, Paper, Box, Card, CardContent, CardMedia, Button, CardActionArea, CardActions, Chip, Avatar } from '@mui/material';
 import { PROJECTS } from '../Constants/ProjectsConstants';
+import { GitHub } from '@mui/icons-material';
 
 const Projects = () => {
   return (
     <>
       <Typography variant='h3'>Here are some projects that I have worked on</Typography>
-      <Box
+      <Grid
         display='flex'
+        flexWrap='wrap'
       >
         <Grid
           container
           direction='row'
-          justifyContent="flex-start"
           alignItems="flex-start"
+          justifyContent="space-around"
         >
           {PROJECTS.map(project => {
             return (
-              <Paper
-                elevation={12}
-                sx={{
-                  width: 300,
-                }}
-              >
-                <Typography>
-                  {project.name}
-                </Typography>
-                <Typography>
-                  {project.techStack.map(language => {
-                    return (
-                      <li>{language}</li>
-                    )
-                  })}
-                </Typography>
-                <Typography>
-                  {project.description}
-                </Typography>
-                <Typography>
-                  {project.link}
-                </Typography>
-                <Typography>
-                  {project.repo}
-                </Typography>
-              </Paper>
+              <Card sx={{
+                maxWidth: 425,
+                margin: 10,
+                borderRadius: 5,
+                backgroundColor: 'rgb(23,58,94)',
+                color: 'white',
+                '&: hover': {
+                  boxShadow: '0px 0px 30px pink'
+                }
+              }}>
+                <CardActionArea
+                  href={project.link}
+                >
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={project.image}
+                    alt={project.name}
+                  />
+                  <CardContent
+                    sx={{
+                      padding: 3,
+                    }}
+                  >
+                    <Typography gutterBottom variant="h5" component="div">
+                      {project.name}
+                    </Typography>
+                    <Typography variant="body2">
+                      {project.techStack.map(language => {
+                        return (
+                          <Chip
+                            color="secondary"
+                            size="small"
+                            label={language}
+                            avatar={<Avatar>{language[0]}</Avatar>}
+                            sx={{
+                              marginRight: 1,
+                              marginBottom: 1
+                            }}
+                          />
+                        )
+                      })}
+                    </Typography>
+                    <Typography variant="body2">
+                      {project.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="block"
+                    color="secondary"
+                    variant="contained"
+                    href={project.repo}
+                    startIcon={<GitHub />}
+                    sx={{
+                      margin: 2,
+                      width: '100%'
+                    }}
+                  >
+                    Repo
+                  </Button>
+                </CardActions>
+              </Card>
             )
           })}
         </Grid>
-      </Box>
+      </Grid>
     </>
   )
 
--- src/Constants/ProjectsConstants.js
+++ src/Constants/ProjectsConstants.js
@@ -1,24 +1,31 @@
+import Write from '../img/Write.jpg';
+import Beer from '../img/Beer.jpg';
+import Space from '../img/Space.jpg';
+
 export const PROJECTS = [
   {
     name: 'No More Cover Letter',
     techStack: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Bootstrap', 'React', 'Redux', 'Firebase'],
     description: 'This application was meant to streamline and de-mistify the process of writing a cover letter for a job appliaciton. A good cover letter can really set you apart from other candidates in the applicant pool and a great way to spice up your cover letter, is to include keywords/power words from the job posting into your cover letter. You can manually do this by scanning the job posting and writing them down as you go, or you can use this application to find them for you. Once this system has identified them, include them in your cover letter, upload it into this application and it will give you a letter grade for how many of those words are a match and if you should apply!',
     link: 'https://no-more-cover-letter.firebaseapp.com/',
-    repo: ''
+    repo: 'https://github.com/JesseDWhite/no-more-cover-letter',
+    image: Write
   },
   {
     name: 'Wish You Were Beer',
     techStack: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Bootstrap', 'React', 'Redux'],
     description: 'This application will allow the user to add kegs to their tap list for customers to buy. Each time a customer buys a pint, the total amount of pints remaining will decrease.',
     link: 'https://wish-you-were-beer.herokuapp.com/',
-    repo: 'https://github.com/JesseDWhite/wish-you-were-beer'
+    repo: 'https://github.com/JesseDWhite/wish-you-were-beer',
+    image: Beer
   },
   {
     name: 'Super Galactic Age Calculator',
     techStack: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Bootstrap'],
     description: 'Use this aplication to see how old you would be on other planets! Put your name and current age into the form and you will see your answer on the right side of the page. You will also see what the average life expectancy is on that planet and how you compare!',
     link: 'https://nostalgic-joliot-c01fcb.netlify.app/',
-    repo: 'https://github.com/JesseDWhite/super-galactic-age-calculator'
+    repo: 'https://github.com/JesseDWhite/super-galactic-age-calculator',
+    image: Space
   },
   // {
   //   name: '',
